81bde4376d4d7fdfd77173899063ab2604dfa43d
3A532051 <chu123520@gmail.com>
2018-12-13 00:10:07 +0800

    修正樣式細節

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,13 +27,16 @@ class LoginController extends Controller
      */
     protected $redirectTo = '/home';
 
-    /**
+
+
+/**
      * Create a new controller instance.
      *
      * @return void
      */
     public function __construct()
     {
+
         $this->middleware('guest')->except('logout');
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -24,7 +24,7 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#">Start Bootstrap</a>
+        <font face="微軟正黑體"><a class="navbar-brand" href="#">中古車體零件</a></font>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
