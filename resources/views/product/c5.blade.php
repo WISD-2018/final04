6ab3435187a67a9385d69a40c5b956e6ce538e40
3A532051 <chu123520@gmail.com>
2018-12-20 23:25:17 +0800

    刪除footer

--- resources/views/product/c5.blade.php
+++ resources/views/product/c5.blade.php
@@ -298,12 +298,7 @@
     <!-- /.container -->
 
     <!-- Footer -->
-    <footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
-      </div>
-      <!-- /.container -->
-    </footer>
+
 
     <!-- Bootstrap core JavaScript -->
     <!--<script src="SecondHand/public/js/jquery.min.js"></script> -->
