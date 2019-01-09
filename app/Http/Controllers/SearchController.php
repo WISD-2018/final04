5c8df685939fd02e69d608ce76939f45ac472d3e
3A532051 <chu123520@gmail.com>
2019-01-09 20:33:31 +0800

    設定搜尋條件不得為空白字串

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -14,14 +14,19 @@ class SearchController extends Controller
     }
     public function search(Request $request){
         $searchword=$request->input('searchword');
+
+        if($searchword != null){
+
         $product=products::where('name','like',"%$searchword%")
             ->orWhere('price','<=',$searchword)
             ->get();
 
-
-
         $data=['products'=>$product];
         return view('product.c7',$data);
+        }
+        else{
+            return redirect()->route('home.index');
+        }
 
     }
 }
