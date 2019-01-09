2dd0866b02a6f9836f654d4a176571aa726b32e9
3A532051 <chu123520@gmail.com>
2019-01-10 00:57:48 +0800

    修改搜尋關鍵字條件為空值或空白字串

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -14,15 +14,18 @@ class SearchController extends Controller
     }
     public function search(Request $request){
         $searchword=$request->input('searchword');
+        $start=trim($searchword);
+        $end=trim($searchword);
 
-        if($searchword != null){
+        if($start != null and $end != null){
 
         $product=products::where('name','like',"%$searchword%")
             ->orWhere('price','<=',$searchword)
             ->get();
-
         $data=['products'=>$product];
+
         return view('product.c7',$data);
+
         }
         else{
             return redirect()->route('home.index');
